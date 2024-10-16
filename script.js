b9cb0d90040aeba817a198f5f80de04a0442d924
George Ouma <georgetieno@gmail.com>
2024-10-16 16:45:40 +0300

    Modified: Script Fetch from HTTP to HTTPs

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.getElementById('searchBtn').addEventListener('click', (event) => {
 const getWeatherData = async (city) => {
     weatherDisplay.innerHTML = '<p>Loading...</p>'; // Loading message
     try {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric`)
 
         if (!response.ok) {
             throw new Error('City not found. Please check the city name and try again.');
