c43fe5fe44a1c5f0d11ed2a8a00a9a9d62e6e968
George Ouma <georgetieno@gmail.com>
2024-10-17 11:23:33 +0300

    Modified: debuging to display error massage

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ document.getElementById('searchBtn').addEventListener('click', (event) => {
     if (city) {
         getWeatherData(city);
     } else {
-        displayError('Please enter a city name');
+        displayError('Enter a city name');
     }
 });
 
 const getWeatherData = async (city) => {
-    weatherDisplay.innerHTML = '<p>Loading...</p>'; // Loading message
+    weatherDisplay.innerHTML = '<p>Loading...</p>'; 
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric`)
 
         if (!response.ok) {
-            throw new Error('City not found, Please check the city name and try again.');
+            throw new Error('City not found. Check the city name and try again.');
         } else {
             const data = await response.json();
             displayWeather(data);
-            document.getElementById('city').value = ''; // Clear input field
+            document.getElementById('city').value = ''; 
         }
         
     } catch (error) {
@@ -33,7 +33,7 @@ const getWeatherData = async (city) => {
 const displayWeather = (data) => {
     const tempCelsius = data.main.temp;
     const humidity = data.main.humidity;
-    const windSpeedKmh = (data.wind.speed * 3.6).toFixed(2); // Convert m/s to km/h
+    const windSpeedKmh = (data.wind.speed * 3.6).toFixed(2);
     const weatherIcon = data.weather[0].icon;
     const weatherDescription = data.weather[0].description;
 
@@ -46,10 +46,10 @@ const displayWeather = (data) => {
         <p>${weatherDescription}</p>
     `;
 
-    // Automatically update weather every 10 minutes
+    // update weather data every 10 minutes
     setTimeout(() => {
         getWeatherData(data.name);
-    }, 600000); // 600000 ms = 10 minutes
+    }, 600000); 
 };
 
 const displayError = (message) => {
