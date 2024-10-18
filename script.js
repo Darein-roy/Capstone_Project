3fa30b07fdb3252239d1721101d7e27057a11688
George Ouma <georgetieno@gmail.com>
2024-10-18 09:29:51 +0300

    Modified: script, added event DOM Content loaded

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-const apiKey = "e4731f172418ed13a2dc73af47f8d4f0";
-const weatherDisplay = document.getElementById('weather-display');
+document.addEventListener("DOMContentLoaded" , () => {
+    const apiKey = "e4731f172418ed13a2dc73af47f8d4f0";
+    const weatherDisplay = document.getElementById('weather-display');
 
-document.getElementById('searchBtn').addEventListener('click', (event) => {
-    event.preventDefault();
+    document.getElementById('searchBtn').addEventListener('click', (event) => {
+        event.preventDefault();
     
-    const city = document.getElementById('city').value.trim();
-    if (city) {
-        getWeatherData(city);
-    } else {
-        displayError('Enter a city name');
-    }
-});
-
-const getWeatherData = async (city) => {
-    weatherDisplay.innerHTML = '<p>Loading...</p>'; 
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric`)
-
-        if (!response.ok) {
-            throw new Error('City not found. Check the city name and try again.');
+        const city = document.getElementById('city').value.trim();
+        if (city) {
+            getWeatherData(city);
         } else {
-            const data = await response.json();
-            displayWeather(data);
-            document.getElementById('city').value = ''; 
+            displayError('Enter a city name');
         }
-        
-    } catch (error) {
-        displayError(error.message);
-    }
-};
+    });
 
-const displayWeather = (data) => {
+    const getWeatherData = async (city) => {
+        weatherDisplay.innerHTML = '<p>Loading...</p>'; 
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric`)
+
+            if (!response.ok) {
+                throw new Error('City not found. Check the city name and try again.');
+            } else {
+                const data = await response.json();
+                displayWeather(data);
+                document.getElementById('city').value = ''; 
+            }
+        
+        } catch (error) {
+            displayError(error.message);
+        }
+    };
+
+    const displayWeather = (data) => {
     const tempCelsius = data.main.temp;
     const humidity = data.main.humidity;
     const windSpeedKmh = (data.wind.speed * 3.6).toFixed(2);
@@ -50,8 +51,10 @@ const displayWeather = (data) => {
     setTimeout(() => {
         getWeatherData(data.name);
     }, 600000); 
-};
+    };
 
-const displayError = (message) => {
-    weatherDisplay.innerHTML = `<p style="color: red;">${message}</p>`;
-};
+    const displayError = (message) => {
+        weatherDisplay.innerHTML = `<p style="color: red;">${message}</p>`;
+    };
+
+});
